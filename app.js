08a3d0d3a54f5e41e91a67ed7189d1d0d474e466
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-05-13 18:08:07 +0530

    set up express in project

---
+++ app.js
@@ -0,0 +1,13 @@
+// It contains Code to run Server
+
+// import statements
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('express is working fine !!');
+});
